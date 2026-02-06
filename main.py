32b2d740d49df2dade999aee76834cb84e6bd248
jkuschner <60696513+jkuschner@users.noreply.github.com>
2026-02-05 21:00:27 -0800

    add AI call loop so the agent can call functions to figure out the answers to user questions

--- main.py
+++ main.py
@@ -20,35 +20,51 @@ if api_key == None:
 
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=messages,
-    config=types.GenerateContentConfig(
-        system_instruction=system_prompt,
-        temperature=0,
-        tools=[available_functions])
-)
+for _ in range(5):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt,
+            temperature=0,
+            tools=[available_functions])
+    )
 
-metadata = response.usage_metadata
-if metadata == None:
-    raise RuntimeError("Failed API Request")
+    if response.candidates:
+        for candidate in response.candidates:
+            if candidate.content:
+                messages.append(candidate.content)
+
+    metadata = response.usage_metadata
+    if metadata == None:
+        raise RuntimeError("Failed API Request")
 
 
-if args.verbose:
-    print(f"User prompt: {args.prompt}")
-    print(f"Prompt tokens: {metadata.prompt_token_count}")
-    print(f"Response tokens: {metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.prompt}")
+        print(f"Prompt tokens: {metadata.prompt_token_count}")
+        print(f"Response tokens: {metadata.candidates_token_count}")
 
-print(f"Response:\n{response.text}")
-if response.function_calls != None:
-    for function_call in response.function_calls:
-        print(f"Calling function: {function_call.name}({function_call.args})")
-        function_call_result = call_function(function_call, verbose=args.verbose)
-        if not function_call_result.parts:
-            raise Exception(f"problem with calling function: {function_call.name} with args: {function_call.args}")
-        if not function_call_result.parts[0].function_response:
-            raise Exception(f"Missing function response when calling function: {function_call.name} with args: {function_call.args}")
-        if not function_call_result.parts[0].function_response.response:
-            raise Exception(f"Missing function response when calling function: {function_call.name} with args: {function_call.args}")
-        if args.verbose:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
+    if response.function_calls:
+        function_responses = []
+
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+            function_call_result = call_function(function_call, verbose=args.verbose)
+            if not function_call_result.parts:
+                raise Exception(f"problem with calling function: {function_call.name} with args: {function_call.args}")
+            if not function_call_result.parts[0].function_response:
+                raise Exception(f"Missing function response when calling function: {function_call.name} with args: {function_call.args}")
+            if not function_call_result.parts[0].function_response.response:
+                raise Exception(f"Missing function response when calling function: {function_call.name} with args: {function_call.args}")
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+
+            function_responses.append(function_call_result.parts[0])
+        messages.append(types.Content(role="user", parts=function_responses))
+    else:
+        print(f"Response:\n{response.text}")
+        exit(0)
+
+print("Exceeded maximum number of AI conversation iterations")
+exit(1)
